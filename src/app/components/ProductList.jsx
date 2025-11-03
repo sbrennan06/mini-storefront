19e71a1c12fed8887c0127e7152e373f593a1dc1
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-02 21:33:53 -0500

    Create product list function

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,1 +1,15 @@
-'use client'
+"use client";
+
+export default function ProductList({ products }) {
+  return (
+    <div>
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} - ${p.price} ({p.category}) stock: {p.stock}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
