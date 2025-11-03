6863143357a849ad32bedfd8343bc4ce3ad12ee5
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-02 22:46:11 -0500

    create pricefilter function

--- src/app/components/PriceFilter.jsx
+++ src/app/components/PriceFilter.jsx
@@ -1,1 +1,18 @@
-'use client'
+"use client";
+
+export default function PriceFilter({ value, onChange }) {
+  return (
+    <div className="price-filter">
+      <label>
+        Max Price:{""}
+        <input
+          type="number"
+          min="0"
+          step="1"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </label>
+    </div>
+  );
+}
